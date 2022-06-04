7523380758099f01bc93a45d6f5a9bdcd6ca505f
OMPedr0_ <59456575+OMPedr0@users.noreply.github.com>
2022-06-04 20:48:16 +0100

    Att Resolve Bug

--- Ticket Liberar/index.js
+++ Ticket Liberar/index.js
@@ -15,12 +15,6 @@ client.discord = Discord;
 client.config = config;
 
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
-};
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
